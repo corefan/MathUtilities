7e5804c5b8c361044528506aaf5b54eb47dd1053
Johnathon Selstad <makeshifted@gmail.com>
2017-05-21 12:26:07 -0700

    IK Environment Collisions

--- Assets/Constraints/IK/CCDIK/CCDIK.cs
+++ Assets/Constraints/IK/CCDIK/CCDIK.cs
@@ -5,9 +5,40 @@ public class CCDIK : MonoBehaviour {
   public Transform Target;
   public CCDIKJoint[] joints;
 
+  public SphereCollider environment;
+  public Transform nearestPoint;
+  public Transform displacementTarget;
+
   void Update() {
-    for (int j = 0; j < joints.Length; j++) {
-      joints[j].Evaluate(Tooltip, Target, j < 2);
+    for (int it = 0; it < 100; it++) {
+      //Move toward main target
+      for (int j = 0; j < joints.Length; j++) {
+        joints[j].Evaluate(Tooltip, Target, 0.01f);
+      }
+
+      //Find the closest point 
+      for (int j = joints.Length - 1; j >= 0; j--) {
+        //for (int j = 0; j < joints.Length; j++) {
+        Vector3 jointPoint = actualCapsuleClosestPoint(environment.transform.position, joints[j].collider as CapsuleCollider);
+        Vector3 offset = jointPoint - environment.transform.position;
+        if (offset.magnitude < environment.radius * environment.transform.lossyScale.x) {
+          nearestPoint.parent = joints[j].transform;
+          nearestPoint.position = jointPoint;
+          displacementTarget.position = nearestPoint.transform.position + (offset.normalized * ((environment.radius * environment.transform.lossyScale.x) - offset.magnitude));//(0.0001f/ (offset.magnitude * offset.magnitude)));
+          Debug.DrawLine(nearestPoint.position, displacementTarget.position);
+
+          //Move away from environment
+          for (int k = j; k < joints.Length; k++) {
+            joints[k].Evaluate(nearestPoint, displacementTarget);
+          }
+        }
+      }
     }
+  }
+
+  Vector3 actualCapsuleClosestPoint(Vector3 point, CapsuleCollider collider) {
+    Vector3 offset = (collider.direction==0?Vector3.right:Vector3.up) * Mathf.Clamp01((collider.height*0.5f) - collider.radius);
+    Vector3 onSegment = Constraints.ConstrainToSegment(point, collider.transform.TransformPoint(collider.center + offset), collider.transform.TransformPoint(collider.center - offset));
+    return onSegment + ((point - onSegment).normalized * collider.radius);
   }
 }
--- Assets/Constraints/IK/CCDIK/CCDIKJoint.cs
+++ Assets/Constraints/IK/CCDIK/CCDIKJoint.cs
@@ -5,9 +5,9 @@ public class CCDIKJoint : MonoBehaviour {
   public float maxAngle = 180;
   Vector3 perpendicular; void Start() { perpendicular = axis.perpendicular(); }
 
-  public void Evaluate(Transform ToolTip, Transform Target, bool rotateToDirection = false) {
+  public void Evaluate(Transform ToolTip, Transform Target, float alpha = 1f) {
     //Rotate the assembly so the tooltip better matches the target position/direction
-    transform.rotation = (rotateToDirection ? Quaternion.FromToRotation(ToolTip.forward, Target.forward) : Quaternion.FromToRotation(ToolTip.position - transform.position, Target.position - transform.position)) * transform.rotation;
+    transform.rotation = Quaternion.Slerp(Quaternion.identity, Quaternion.FromToRotation(ToolTip.position - transform.position, Target.position - transform.position), alpha) * transform.rotation;
 
     //Enforce only rotating with the hinge
     transform.rotation = Quaternion.FromToRotation(transform.rotation * axis, transform.parent.rotation * axis) * transform.rotation;
